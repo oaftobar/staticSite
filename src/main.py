db86fb77baa1f33a73ad0e47235d56ac432a7f3c
oaftobar <oaftobar@users.noreply.github.com>
2026-05-02 00:29:51 -0400

    Update main.py w/better basepath logic

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
-import sys
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from generate import generate_pages_recursive
 
-basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
 dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
